c566d5cab333c1e8ead2039526b7a711d151cbde
Leonardo <leonardoorooo@gmail.com>
2022-03-13 13:54:02 -0300

    criacao de calculadora

---
+++ dominando-ides-java/src/br/com/calculadora/Calculadora.java
@@ -0,0 +1,45 @@
+package br.com.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		double divisao = divisao(a,b);
+		int multiplicao = multiplicao(a,b);
+		
+		System.out.println(soma);
+		System.out.println(subtracao);
+		System.out.println(divisao);
+		System.out.println(multiplicao);
+		
+		
+
+	}
+	
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+	public static double divisao(double a, double b) {
+		return a / b;
+	}
+	public static int multiplicao(int a, int b) {
+		return a * b;
+	}
+
+}
